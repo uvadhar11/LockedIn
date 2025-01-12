bf2a6391ca149eff08903d9b691ae442e94dfd45
Umang Vadhar <umang.vadhar@gmail.com>
2025-01-11 19:37:42 -0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="bg-blue-500 text-white p-4">Hello world!</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // tells tailwind where our index.html file and src files are (in case we move it outside the default file placement)
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    // overriding default files
+    // fontFamily: {
+    //   // sans is default font injected by tailwind and then by overriding this value, we are setting every font to roboto mono
+    //   sans: "Roboto Mono, monospace",
+    // },
+    // extends the basic styles (can see them in documentation and github link for the default files and object stuff). can add stuff by putting it into extend, but if you put something that has an already existing name then it will override it.
+    extend: {
+      // so now pizza is a color you can use
+      // colors: {
+      //   pizza: "#123456",
+      // },
+      // ensures viewport height is 100% because sometimes its not
+      // screen: {
+      //   height: "100dvh",
+      // },
+    },
+    plugins: [],
+  },
+};
